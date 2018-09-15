29417936252fb6f03353f363b1e7c7b83a9e72e2
unknown <mkural@mit.edu>
2018-09-15 14:13:11 -0400

    Add notifications and storage

--- background.js
+++ background.js
@@ -2,6 +2,9 @@ chrome.runtime.onInstalled.addListener(function() {
 chrome.storage.sync.set({color: '#3aa757'}, function() {
   console.log('The color is green.');
 });
+chrome.storage.sync.set({num_swears: 0}, function(){
+  console.log("Number of swears is initialized to 0.");
+});
 chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
   chrome.declarativeContent.onPageChanged.addRules([{
     conditions: [new chrome.declarativeContent.PageStateMatcher({
--- popup.js
+++ popup.js
@@ -5,6 +5,21 @@ changeColor.style.backgroundColor = data.color;
 changeColor.setAttribute('value', data.color);
 });
 changeColor.onclick = function(element) {
+chrome.storage.sync.get(['num_swears'], function(result){
+  inc_result = result.num_swears+1;
+  
+  chrome.storage.sync.set({num_swears: inc_result}, function(){
+  	console.log("Increment number of swears to " + inc_result);
+  });
+  var opt = {
+    type: "basic",
+    title: "Not in my Christian browser!",
+    message: "Stop swearing! what the fuck? you swore " + inc_result + " times!",
+    iconUrl: "images/disappoint.jpg"
+  }
+  chrome.notifications.create(opt);
+});
+
 let color = element.target.value;
 chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
   chrome.tabs.executeScript(
