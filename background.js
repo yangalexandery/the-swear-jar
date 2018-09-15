a74899a2809f8a183813ec7dfad484f9d24eaf7a
yangalexandery <yangalexandery@gmail.com>
2018-09-15 13:51:53 -0400

    added 'possibleSwears', key-value dictionaries where values may contain swear words

--- background.js
+++ background.js
@@ -1,3 +1,18 @@
+var parseRawData = function(data) {
+	// data is 
+	// console.log(data[d]['bytes']);
+	uintarray = data[0]['bytes'];
+	uintarray = new Uint8Array(uintarray);
+	stringdata = new TextDecoder("utf-8").decode(uintarray);
+	body = stringdata.split('&');
+	body_dict = {}
+	for (i in body) {
+		str_split = body[i].split('=');
+		body_dict[str_split[0]] = str_split[1];
+	}
+	return body_dict;
+};
+
 chrome.runtime.onInstalled.addListener(function() {
 	chrome.storage.sync.set({color: '#3aa757'}, function() {
 	  console.log('The color is green.');
@@ -12,7 +27,31 @@ chrome.runtime.onInstalled.addListener(function() {
 	  }]);
 	});
 	chrome.webRequest.onBeforeRequest.addListener(
-  		function(details){console.log(details.url);},
-	  	{urls: ["<all_urls>"]}
+  		function(details){
+  			// if ('requestBody' in details) {
+  			// 	console.log("good");
+  			// 	console.log(details.url);
+  			// 	console.log(details.requestBody.raw);
+  			// 	parseRawData(details.requestBody.raw);
+  			// }
+  			if ('requestBody' in details) {
+  				if ('formData' in details.requestBody) {
+  					console.log("Form data: ")
+					var possibleSwears = details.requestBody.formData
+					console.log(possibleSwears);
+  				} else if ('raw' in details.requestBody) {
+  					var possibleSwears = parseRawData(details.requestBody.raw);
+  					console.log("Form data: ")
+  					console.log(possibleSwears);
+  				}
+  			}
+  			if ('url' in details) {
+  				console.log("URL params: ")
+  				var possibleSwears = new URLSearchParams(details.url);
+  				console.log(possibleSwears);
+  			}
+  		},
+	  	{urls: ["<all_urls>"]},
+	  	["requestBody"]
 	);
 });
