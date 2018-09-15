844a62d9648ad2965f33fc2da5b66503857f46ef
Linda <lgong@mit.edu>
2018-09-15 16:56:29 -0400

    removed color stuff

--- popup.js
+++ popup.js
@@ -25,10 +25,10 @@ changeColor.setAttribute('value', data.color);
 // // });
 // });
 
-let color = element.target.value;
-chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-  chrome.tabs.executeScript(
-      tabs[0].id,
-      {code: 'document.body.style.backgroundColor = "' + color + '";'});
-});
-};
+// let color = element.target.value;
+// chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+//   chrome.tabs.executeScript(
+//       tabs[0].id,
+//       {code: 'document.body.style.backgroundColor = "' + color + '";'});
+// });
+// };
