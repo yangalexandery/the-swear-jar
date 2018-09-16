a3f91da0bba5f8008a1d35965caedc362c1fbd6d
yangalexandery <yangalexandery@gmail.com>
2018-09-16 02:42:42 -0400

    finishing touches

--- emptiness.js
+++ emptiness.js
@@ -2,11 +2,17 @@ console.log("Emptiness.js active");
 var updateState = function(isFull){
     var jarMessage = document.getElementById("jar-message");
     var jarImage = document.getElementById("jar-image");
+    var soapMessage1 = document.getElementById("soap-message-1");
+    var soapMessage2 = document.getElementById("soap-message-2");
+    var soapMessage3 = document.getElementById("soap-message-3");
     console.log(jarMessage);
     console.log(jarImage);
     if(isFull) {
         jarMessage.innerHTML = "Uh oh! Your swear jar is full!";
         jarImage.src = "images/jar-full.jpg";
+        soapMessage1.innerHTML = "You've been saying way too many naughty words.";
+        soapMessage2.innerHTML = "Luckily, the money in the swear jar is just enough to pay for a";
+        soapMessage3.innerHTML = "bar of (virtual) soap, to wash out that filthy mouth of yours.";
     }
 }
 
